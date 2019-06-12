af95c7d6f1f56ed1fa5d8cbe52f91f911dacb49c
Crypto347 <crypto347@gmail.com>
2019-06-12 22:35:18 +0400

    SimpleArray

--- src/components/app.js
+++ src/components/app.js
@@ -3,14 +3,20 @@ import React,{
 } from 'react';
 import List from "./list";
 import Form from "./form";
-import './app.scss'
+
+import DataGrid, { Column, FilterRow, HeaderFilter, SearchPanel } from 'devextreme-react/data-grid';
+import { SelectBox, CheckBox } from 'devextreme-react';
+import { customers } from './data.js';
 
 export class App extends Component {
    render(){
       return(
          <div className="abc">
-               <h2>{"JJnJ"}</h2>
-               <Form/>
+                 <DataGrid
+                        dataSource={customers}
+                        columns={['CompanyName', 'City', 'State', 'Phone', 'Fax']}
+                        showBorders={true}
+                  />
          </div>
       );
    }
