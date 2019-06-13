d5a9ddfa13d93ceb8ad42d2a4d100c089a00ae10
Crypto347 <crypto347@gmail.com>
2019-06-13 07:34:59 +0400

    SimpleArray with pages

--- src/components/app.js
+++ src/components/app.js
@@ -3,20 +3,39 @@ import React,{
 } from 'react';
 import List from "./list";
 import Form from "./form";
+import './app.scss';
 
-import DataGrid, { Column, FilterRow, HeaderFilter, SearchPanel } from 'devextreme-react/data-grid';
+import DataGrid, { Column, FilterRow, HeaderFilter, SearchPanel, Pager, Paging  } from 'devextreme-react/data-grid';
 import { SelectBox, CheckBox } from 'devextreme-react';
 import { customers } from './data.js';
 
 export class App extends Component {
    render(){
       return(
-         <div className="abc">
+         <div>
                  <DataGrid
                         dataSource={customers}
                         columns={['CompanyName', 'City', 'State', 'Phone', 'Fax']}
                         showBorders={true}
                   />
+                  <div className="space"/>
+                  <DataGrid
+                        dataSource={customers}
+                        showBorders={true}
+                        >
+                        <Paging defaultPageSize={10} />
+                        <Pager
+                        showPageSizeSelector={true}
+                        allowedPageSizes={[5, 10, 20]}
+                        showInfo={true} />
+
+                        <Column dataField={'CompanyName'} />
+                        <Column dataField={'City'} />
+                        <Column dataField={'State'} />
+                        <Column dataField={'Phone'} />
+                        <Column dataField={'Fax'} />
+                  </DataGrid>
+                  <div className="space"/>
          </div>
       );
    }
